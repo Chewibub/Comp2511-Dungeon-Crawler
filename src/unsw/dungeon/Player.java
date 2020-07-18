ca4ebc1cdc54cf5fdfe44f77f965818796fa5f29
Kelvin Lun <chewibub@gmail.com>
2020-07-18 20:19:26 +1000

    Added observers and some stubs

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -1,5 +1,8 @@
 package unsw.dungeon;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * The player entity
  * @author Robert Clifton-Everest
@@ -8,6 +11,7 @@ package unsw.dungeon;
 public class Player extends Entity {
 
     private Dungeon dungeon;
+    private StringProperty direction;
 
     /**
      * Create a player positioned in square (x,y)
@@ -15,27 +19,55 @@ public class Player extends Entity {
      * @param y
      */
     public Player(Dungeon dungeon, int x, int y) {
-        super(x, y);
+        super(x, y, "Player");
         this.dungeon = dungeon;
     }
 
     public void moveUp() {
-        if (getY() > 0)
-            y().set(getY() - 1);
+        int newY = getY() - 1;
+        int newX = getX();
+        if (!dungeon.validMove(this, newX, newY)) {
+            return;
+        } else if (getY() > 0) {
+            y().set(newY);
+        }
+        direction = new SimpleStringProperty("Up");
+        pingObservers();
     }
 
     public void moveDown() {
-        if (getY() < dungeon.getHeight() - 1)
-            y().set(getY() + 1);
+        int newY = getY() + 1;
+        int newX = getX();
+        if (!dungeon.validMove(this, newX, newY)) {
+            return;
+        } else if (getY() < dungeon.getHeight() - 1) {
+            y().set(newY);
+        }
+        direction = new SimpleStringProperty("Down");
+        pingObservers();
     }
 
     public void moveLeft() {
-        if (getX() > 0)
-            x().set(getX() - 1);
+        int newY = getY();
+        int newX = getX() - 1;
+        if (!dungeon.validMove(this, newX, newY)) {
+            return;
+        } else if (getX() > 0) {
+            x().set(newX);
+        }
+        direction = new SimpleStringProperty("Left");            
+        pingObservers();
     }
 
     public void moveRight() {
-        if (getX() < dungeon.getWidth() - 1)
-            x().set(getX() + 1);
+        int newY = getY();
+        int newX = getX() + 1;
+        if (!dungeon.validMove(this, newX, newY)) {
+            return;
+        } else if (getX() < dungeon.getWidth() - 1) {
+            x().set(newX);
+        }
+        direction = new SimpleStringProperty("Right");
+        pingObservers();
     }
 }
