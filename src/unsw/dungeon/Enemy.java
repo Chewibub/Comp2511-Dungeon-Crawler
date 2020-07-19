a979719b122c0024745cf17160e80ed2fad00e89
Macauley Dallas <macauley.dallas@student.unsw.edu.au>
2020-07-19 03:13:56 -0700

    Added basic enemy with movement

---
+++ src/unsw/dungeon/Enemy.java
@@ -0,0 +1,41 @@
+package unsw.dungeon;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * The Enemy entity
+ * Moves left to right
+ */
+
+public class Enemy extends Entity {
+
+    private Dungeon dungeon;
+    private int direction;
+    /**
+     * Create a enemy positioned in square (x,y)
+     * @param x
+     * @param y
+     */
+
+    public Enemy(Dungeon dungeon, int x, int y) {
+        super(x, y, "Enemy");
+        this.dungeon = dungeon;
+        this.direction = 1;
+    }
+    public void triggerMovement() {
+        move(direction);
+    }
+
+    public void move(int d) {
+        int newY = getY();
+        int newX = getX() + d;
+        if (dungeon.validMove(this, newX, newY)) {
+            x().set(newX);
+        } else {
+            direction = d * -1;
+            return;
+        }
+            
+    }
+}
