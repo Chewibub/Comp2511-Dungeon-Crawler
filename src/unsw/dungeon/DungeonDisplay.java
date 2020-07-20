2fc99cdeaf7b261cf6adeaa93152f8ae4245e360
Zijing Zhang <zijing.zhang@student.unsw.edu.au>
2020-07-21 01:08:42 +1000

    Replace DungeonDisplay.java

--- src/unsw/dungeon/DungeonDisplay.java
+++ src/unsw/dungeon/DungeonDisplay.java
@@ -2,8 +2,6 @@ package unsw.dungeon;
 
 import java.util.List;
 
-import javafx.application.Platform;
-
 public class DungeonDisplay implements DungeonObserver {
     Dungeon dungeon;
     Player player;
@@ -19,39 +17,36 @@ public class DungeonDisplay implements DungeonObserver {
         List<Entity> entities = dungeon.getEntities();
         for (Entity temp : entities) {
             if (temp != null) {
-                if (temp.collides(player) == true) {
-                    temp.smash();
+                if (temp.getType().equals("Enemy")) {
+                    Enemy enemy= (Enemy) temp;
+                    enemy.triggerMovement();
                 }
             }
         }
+
+        for (Entity temp : entities) {
+            if (temp != null) {
+                if (temp.collides(player)) {
+                    temp.smash();
+                }
+
+            }
+        }
+
         List<Goal> goals = dungeon.getGoals();
-        String conditional = "ONE";
         for (Goal temp : goals) {
             if (temp != null) {
                 temp.updateGoal();
-                conditional = temp.getCondition();
                 if (temp.checkCompleted() == true && temp.getCondition().equals("ONE")) {
                     System.out.println("You won!!");
-                    Platform.exit();
-                    break;
-                } else if (temp.checkCompleted() == true && temp.getCondition().equals("OR")) {
-                    System.out.println("You won!!");
-                    Platform.exit();
-                    break;
-                } else if (temp.getCondition().equals("AND")) {
-                    if (temp.checkCompleted() == false) {
-                        return;
-                    }
                 }
             }
         }
-        if (conditional.equals("AND")) {
-            System.out.println("You won!!");
-            Platform.exit();            
-        }
+
     }
 
-    public Entity getEntity() {
-        return player;
-    }
+//    @Override
+//    public Entity getEntity() {
+//        return player;
+//    }
 }
