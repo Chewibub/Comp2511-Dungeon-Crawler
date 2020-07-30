3165443062dd38190ca7dc6bf215c26746d9bc68
Macauley Dallas <macauley.dallas@student.unsw.edu.au>
2020-07-30 12:23:05 -0700

    Removing srcold

--- srcold/unsw/dungeon/DungeonObserver.java
+++
@@ -1,6 +0,0 @@
-package unsw.dungeon;
-
-public interface DungeonObserver {
-	public void update();
-	// public Entity getEntity();
-}
--- srcold/unsw/dungeon/State.java
+++
@@ -1,5 +0,0 @@
-package unsw.dungeon;
-
-public enum State {
-    ON, OFF,
-}
