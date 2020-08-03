1c0dd381380898a12de4a83394287c14ad2126f9
Macauley Dallas <macauley.dallas@student.unsw.edu.au>
2020-08-03 06:08:18 -0700

    added javadoc

--- src/unsw/dungeon/DungeonApplication.java
+++ src/unsw/dungeon/DungeonApplication.java
@@ -19,7 +19,7 @@ public class DungeonApplication extends Application {
         DungeonApplication.primaryStage = primaryStage;
         DungeonApplication.levelStatus = new boolean[6];
         DungeonApplication.levelStatus[0] = true;
-        String[] dunBuff = {"advanced.json", "advanced.json", "advanced.json", "advanced.json", "advanced.json", "advanced.json"};
+        String[] dunBuff = {"level3.json", "level2.json", "level3.json", "advanced.json", "advanced.json", "advanced.json"};
         DungeonApplication.dungeons = dunBuff;
         DungeonApplication.gold = 5;
 
@@ -30,26 +30,57 @@ public class DungeonApplication extends Application {
         primaryStage.show();
     } 
     
+    /**
+     * Allows access to the primary stage via dungeonapplication class.
+     * 
+     * @return static primaryStage variable
+     */
     public static Stage getPrimaryStage() {
         return primaryStage;
     }
 
+    /**
+     * Returns the value of gold player currently has.
+     * @return value stored in static variable gold.
+     */
     public static int getGold() {
         return gold;
     }
-
+    
+    /**
+     * Changes the value of gold the player currently has.
+     * @param value value to increse the gold ammount buy.
+     */
     public static void giveGold(int value) {
         DungeonApplication.gold = DungeonApplication.gold + value;
     }
 
+    /**
+     * Checks if a level is unlocked yet.
+     * 
+     * @param index the index of the level we are checking.
+     * @return the value stored in the levelStatus array at the sepcified index.
+     */
     public static boolean getLevelStatus(int index) {
         return levelStatus[index];
     }
 
+     /**
+     * Unlocks level.
+     * 
+     * @param index the index of the level we are changing.
+     * @param status the status that we are changing the level to.
+     */
     public static void setLevelStatus(int index, boolean status) {
         DungeonApplication.levelStatus[index] = status;
     }
 
+    /**
+     * Retreives the filepath to the dungeon json file
+     * 
+     * @param index of dungeon file string to retreive.
+     * @return dungeon file string stored at location of index.
+     */
     public static String getDungeon(int index) {
         return dungeons[index];
     }
